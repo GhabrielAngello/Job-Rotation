445b0b24e8aa801588c818435419d9aee19201fe
Ghabriel Angello <ghabrielangelo13@gmail.com>
2023-04-21 12:22:29 -0300

    estagio

---
+++ seq_fibonacci.py
@@ -0,0 +1,23 @@
+seq_a = [1, 3, 5, 7]
+prox_num_a = seq_a[-1] + 2
+print(prox_num_a)
+
+seq_b = [2, 4, 8, 16, 32, 64]
+prox_num_b = seq_b[-1] * 2
+print(prox_num_b)
+
+seq_c = [0, 1, 4, 9, 16, 25, 36]
+prox_num_c = (len(seq_c))**2
+print(prox_num_c)
+
+seq_d = [4, 16, 36, 64]
+prox_num_d = (len(seq_d) * 2 + 1)**2
+print(prox_num_d)
+
+seq_e = [1, 1, 2, 3, 5, 8]
+prox_num_e = seq_e[-1] + seq_e[-2]
+print(prox_num_e)
+
+seq_f = [2, 10, 12, 16, 17, 18, 19]
+prox_num_f = seq_f[-1] + 1
+print(prox_num_f)
